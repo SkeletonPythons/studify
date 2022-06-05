f09736c13e44f114078df81b0268e28182ad61f5
Adonis Santos <assantos@student.fullsail.edu>
2022-06-05 17:02:19 -0400

    Added TabBar to Scaffold

--- lib/views/pages/home_page/home_page.dart
+++ lib/views/pages/home_page/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import'../../../services/auth.dart';
+import '../../../services/auth.dart';
 import '../../../controllers/home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,54 +10,53 @@ class HomePage extends StatefulWidget {
   HomePageState createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller = TabController(length: 4, vsync: this);
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery
-        .of(context)
-        .size;
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SizedBox(
-        height: MediaQuery
-            .of(context)
-            .size
-            .height,
-        width: MediaQuery
-            .of(context)
-            .size
-            .width,
-        child: Stack(
-            children: <Widget>[
-        Container(
-        height: size.height * .3,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                      alignment: Alignment.topCenter,
-                      image: AssetImage('assets/dashboard_header.png'),
-                    ),
-                ),
-            ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    height:64,
-                    child:Row(
-                      children:<Widget>[
-                        CircleAvatar(
-                          onBackgroundImageError: (value,trace){},
-                          backgroundImage: NetworkImage( Auth.instance.USER.photoUrl!),
-                        )
-                      ]
-                    )
-                  )
-                ]
-              )
-            ]
+      bottomNavigationBar: Container(
+        color: Color(0xff414141),
+        height: MediaQuery.of(context).size.height * .1,
+        width: MediaQuery.of(context).size.width,
+        child: TabBar(
+          tabs: <Widget>[
+            Container(child: Icon(Icons.home)),
+            Container(child: Icon(Icons.flash_on)),
+            Container(),
+            Container()
+          ],
+          controller: controller,
         ),
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(children: <Widget>[
+          Container(
+            height: size.height * .3,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                alignment: Alignment.topCenter,
+                image: AssetImage('assets/dashboard_header.png'),
+              ),
+            ),
+          ),
+          Column(children: <Widget>[
+            Container(
+                height: 64,
+                child: Row(children: <Widget>[
+                  // CircleAvatar(
+                  //   onBackgroundImageError: (value, trace) {},
+                  //   backgroundImage: NetworkImage(Auth.instance.USER.photoUrl!),
+                  // )
+                ]))
+          ])
+        ]),
       ),
     );
   }
 }
-
-
