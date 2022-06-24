00d1de951f4074a69476b18a60e20815fca19e76
Adonis Santos <assantos@student.fullsail.edu>
2022-06-24 01:02:10 -0400

    Added "add event" page Added floating action bubble library Add "add event" to routes Added form builder for add event logic

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,15 +1,15 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: prefer_const_constructors
 import 'package:get/get.dart';
-import 'package:studify/views/pages/calendar_page/calendar_page.dart';
 import 'package:studify/views/pages/dashboard_page/dashboard_page.dart';
 import 'package:studify/views/pages/timers_page/pomodoro.dart';
 import '../../../views/pages/splash_page/splash_page.dart';
 import '../../../views/pages/login_page/login_page.dart';
 import '../../../views/pages/timers_page/timer_homepage.dart';
 import '../../../views/pages/bottom_nav_page/bottom_nav_bar.dart';
+import 'package:studify/views/pages/calendar_page/calendar_page.dart';
+import '../views/pages/calendar_page/add_event.dart';
 import '../views/pages/timers_page/timer_pomodoro_setup.dart';
-
 
 abstract class Routes {
   static const String LOGIN = '/login';
@@ -18,6 +18,7 @@ abstract class Routes {
   static const String TIMER = '/timer';
   static const String NAVBAR = '/navbar';
   static const String CALENDAR = '/calendar';
+  static const String ADDEVENT = '/add_event';
   static const String POMODOROSETUP = '/pomodoro_setup';
   static const String POMODORO = '/pomodoro';
 }
@@ -51,6 +52,10 @@ class AppPages {
     GetPage(
       name: Routes.CALENDAR,
       page: () => CalendarPage(),
+    ),
+    GetPage(
+      name: Routes.ADDEVENT,
+      page: () => AddEvent(),
     ),
     GetPage(
       name: Routes.POMODOROSETUP,
