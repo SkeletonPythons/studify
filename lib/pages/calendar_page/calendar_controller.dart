4cff22e0c8530fcdec9b97b330a526b1377df8b0
Adonis Santos <assantos@student.fullsail.edu>
2022-07-14 05:55:14 -0400

    Removed unnesscary files Implemeted some code via VS to test on AS

--- lib/pages/calendar_page/calendar_controller.dart
+++ lib/pages/calendar_page/calendar_controller.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: constant_field_initializers, prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import '../../../models/calendar_model.dart';
@@ -6,15 +9,18 @@ import '../../../services/db.dart';
 
 class CalendarController extends GetxController {
   late final Event newEvent;
+  List<Event> events = <Event>[].obs;
 
   Event createEvent({
     String title = 'New Event',
     String? description = '',
     DateTime? startDate,
-    DateTime? endDate,
+    required DateTime? endDate,
     String? location = '',
     bool isAllDay = false,
   }) {
+    Event event = Event(title: 'New event', endDate: endDate);
+    event.startDate = DateTime.now();
     return Event(
       title: newEvent.title,
       description: '${newEvent.description}',
@@ -24,14 +30,56 @@ class CalendarController extends GetxController {
       isAllDay: isAllDay,
     );
   }
+
+  void toggleAllDay(Event event) async {
+    try {
+      Map<String, dynamic> data = await DB.instance.store
+          .collection('users')
+          .doc(Auth.instance.USER.uid)
+          .collection('events')
+          .doc('event.id')
+          .get() as Map<String, dynamic>;
+
+      bool current = data['isAllDay'];
+
+      await DB.instance.store
+          .collection('users')
+          .doc(Auth.instance.USER.uid)
+          .collection('events')
+          .doc('event.id')
+          .update({'isAllDay': !current});
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  addNewEvenToList(List<Event> events) {
+    events.add(createEvent(
+        title: '',
+        description: '',
+        startDate: DateTime.now(),
+        endDate: DateTime.now().add(Duration(days: 1)),
+        location: '',
+        isAllDay: false));
+  }
 }
 
 class EventController extends GetxController {
   late final Event event;
   late RxBool editEnabled = RxBool(false);
 
-  void setEvent(Event event) {
-    this.event = event;
+  saveEvent(Event event) async {
+    try {
+      await DB.instance.store
+          .collection('users')
+          .doc(Auth.instance.USER.uid)
+          .collection('events')
+          .doc(event.id)
+          .set(event.toMap());
+      SetOptions(merge: true);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   void deleteEvent(Event event) async {
